07ef633b6ba1ffc23bae2a9b6aeeb4ce227654d4
Anuj Dev <an739753@dal.ca>
2022-03-16 16:30:08 -0300

    Project Setup

--- Backend/src/routes/userRoute.js
+++ Backend/src/routes/userRoute.js
@@ -5,9 +5,4 @@ const { userRoot } = require("../controllers/userController");
 router.get("/", userRoot);
 // router.get("/register", rootPage);
 
-// router.get("/users", getAllUsers);
-// router.post("/add", addUser);
-// router.get("/user/:id", getUserById);
-// router.put("/update/:id", updateUser);
-
 module.exports = router;
