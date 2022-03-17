385fbc64cf4890ad7f61b105798523dc8f6aef34
Pankti Vyas <pn695271@dal.ca>
2022-03-17 14:20:56 -0300

    delete service by id api added.

--- Backend/src/controllers/serviceController.js
+++ Backend/src/controllers/serviceController.js
@@ -1,29 +1,29 @@
-const { services } = require("../models"); 
+const { services } = require("../models");
 const { use } = require("../routes/serviceRoute");
 
 const serviceRoot = (req, res) => {
-  try {
-    res.setHeader("Content_type", "application/json");
-    res
-      .status(200)
-      .json({ message: "Welcome to service Module", success: true });
-  } catch (error) {
-    res.status(500).json({ message: error.message, success: false });
-  }
+    try {
+        res.setHeader("Content_type", "application/json");
+        res
+            .status(200)
+            .json({ message: "Welcome to service Module", success: true });
+    } catch (error) {
+        res.status(500).json({ message: error.message, success: false });
+    }
 };
 
-const getAllServices = async (req,res) => {
-    try{
+const getAllServices = async (req, res) => {
+    try {
         const listOfServices = await services.findAll();
-        res.setHeader("Content_type", "application/json"); 
+        res.setHeader("Content_type", "application/json");
         res.status(200).json(listOfServices);
     } catch (error) {
         res.status(500).json({ message: error.message, success: false });
     }
 }
 
-const addService =  async (req,res) => {
-    try{
+const addService = async (req, res) => {
+    try {
         const s = req.body;
         await services.create(s).then(() => {
             res.status(200).json(s);
@@ -33,30 +33,46 @@ const addService =  async (req,res) => {
     }
 }
 
-const editService = async (req,res) => {
-    try{
+const editService = async (req, res) => {
+    try {
         const id = req.params.id;
         await services.update(req.body, {
-            where:{
+            where: {
                 id: id
             }
         })
-        const s = await services.findByPk(id); 
+        const s = await services.findByPk(id);
         res.status(200).json(s);
     } catch (error) {
         res.status(500).json({ message: error.message, success: false });
     }
 }
 
-const getService = async (req,res) => {
-    try{
-        const id= req.params.id;
+const getService = async (req, res) => {
+    try {
+        const id = req.params.id;
         const s = await services.findByPk(id);
         res.status(200).json(s);
     } catch (error) {
         res.status(500).json({ message: error.message, success: false });
-    }    
+    }
+}
+
+const deleteService = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const s = await services.findByPk(id);
+        await services.destroy({
+            where: {
+                id:id
+            }
+        }).then(() => {
+            res.status(200).json(s);
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message, success: false });
+    }
 }
 
 module.exports = { serviceRoot };
-module.exports = { getAllServices, addService, editService, getService };
+module.exports = { getAllServices, addService, editService, getService, deleteService };
--- Backend/src/routes/serviceRoute.js
+++ Backend/src/routes/serviceRoute.js
@@ -4,11 +4,13 @@ const { getAllServices } = require("../controllers/serviceController");
 const { addService } = require("../controllers/serviceController");
 const { editService } = require("../controllers/serviceController");
 const { getService } = require("../controllers/serviceController");
+const { deleteService } = require("../controllers/serviceController");
 
 router.get("/", getAllServices);
 router.get("/:id",getService);
 router.post("/",addService);
 router.put("/:id",editService);
+router.delete("/:id",deleteService);
 
 
 module.exports = router;
