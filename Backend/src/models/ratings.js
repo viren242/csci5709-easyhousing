533bd23511bf45141b642e8a16e2831344cbbb8a
Arvinder Singh Arvinder Singh <ar345438@dal.ca>
2022-03-24 05:13:28 -0300

    committing backend code

---
+++ Backend/src/models/ratings.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const Ratings = sequelize.define('Ratings', {
+        rating_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        property_id: {
+            type: DataTypes.INTEGER,
+            required: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            required: true
+        },
+        rating: {
+            type: DataTypes.DECIMAL(10,6)
+        }
+    });
+    return Ratings;
+}
